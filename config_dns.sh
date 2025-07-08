722876cbb232421d8e9911facbfd0cffe0c9ea7a
cccccrius <crius38@gmail.com>
2025-07-08 09:42:25 +0200

    Update config_dns.sh

--- config_dns.sh
+++ config_dns.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #Init
 
-sudo tee -a /etc/systemd/resolved.conf >/dev/null <<'EOF'
+#sudo tee -a /etc/systemd/resolved.conf >/dev/null <<'EOF'
 #[Resolve]
-DNS=94.140.14.14 2a10:50c0::ad1:ff 94.140.15.15 2a10:50c0::ad2:ff
-DNSOverTLS=yes
-DNSSEC=yes
-FallbackDNS=45.90.28.181 2a07:a8c0::c8:d79a 45.90.30.181 2a07:a8c1::c8:d79a
-EOF
+#DNS=94.140.14.14 2a10:50c0::ad1:ff 94.140.15.15 2a10:50c0::ad2:ff
+#DNSOverTLS=yes
+#DNSSEC=yes
+#FallbackDNS=45.90.28.181 2a07:a8c0::c8:d79a 45.90.30.181 2a07:a8c1::c8:d79a
+#EOF
 
-sudo systemctl restart systemd-resolved
-sudo systemctl restart systemd-networkd
+#sudo systemctl restart systemd-resolved
+#sudo systemctl restart systemd-networkd
 
 nmcli -g name,type connection  show  --active | awk -F: '/ethernet|wireless/ { print $1 }' | while read connection
 do
